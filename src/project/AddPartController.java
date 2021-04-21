eac5eca1f3260aad7a8099321b17e41236c21297
Antonio Peza <anthony@aptechauto.com>
2021-04-21 13:42:39 -0700

    Working with windows

---
+++ src/project/AddPartController.java
@@ -0,0 +1,32 @@
+package project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddPartController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    // This function is the button that will change from one window to the next window
+    public void toSecond(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/ModifyPart.fxml"));
+        Stage stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Second Window");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("view/Main.fxml"));
+        primaryStage.setTitle("Welcome");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
